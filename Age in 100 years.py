181bda252a7cda5a96ffe63dea55a7d5f4a97a57
MirekuCharles <103150839+MirekuCharles@users.noreply.github.com>
2024-01-06 23:31:37 +0000

    Add files via upload

---
+++ Age in 100 years.py
@@ -0,0 +1,6 @@
+user_name = str(input("Your name: "))
+user_age = int(input("Your age: "))
+
+age_in_100_years = 100 - user_age
+
+print(user_name + " will turn 100 in "+ str(age_in_100_years) + " years ")
